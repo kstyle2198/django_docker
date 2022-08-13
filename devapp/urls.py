7b7eacede5fc7ee1f2d605dd805144f4fad348c2
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2022-08-13 19:06:42 +0900

    develop covid plotly

--- devapp/urls.py
+++ devapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dev1, name='dev1'),
+    path('', views.base, name='testbase'),
+    path('dev1/', views.dev1, name='dev1'),
+    path('dev2/', views.dev2, name='dev2'),
+    path('dev3/', views.dev3, name='dev3'),
 ]
--- devapp/views.py
+++ devapp/views.py
@@ -5,39 +5,46 @@ import time
 import requests
 import asyncio
 
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
 # Synchronous
-# def dev1(request):    # 약 17~18초 소요됨
-    # starting_time = time.time()
-    # pokemon_data = []
+def dev1(request):    # 약 17~18초 소요됨
+    starting_time = time.time()
+    pokemon_data = []
     
-    # for num in range(1, 101):
-    #     url = f"https://pokeapi.co/api/v2/pokemon/{num}"
-    #     res = requests.get(url)
-    #     pokemon = res.json()
-    #     pokemon_data.append([pokemon['name'], pokemon['sprites']['other']['official-artwork']['front_default']])
+    for num in range(1, 101):
+        url = f"https://pokeapi.co/api/v2/pokemon/{num}"
+        res = requests.get(url)
+        pokemon = res.json()
+        pokemon_data.append([pokemon['name'], pokemon['sprites']['other']['official-artwork']['front_default']])
     
-    # count = len(pokemon_data)
-    # total_time = time.time()- starting_time
+    count = len(pokemon_data)
+    total_time = time.time()- starting_time
     
-    # context = {'data': pokemon_data, 'count': count, 'time': total_time}
+    context = {'data': pokemon_data, 'count': count, 'time': total_time}
     
-    # return render(request, 'index.html', context)
+    return render(request, 'index.html', context)
+
 
 # Asynchronous
-# async def dev1(request):     # 약 6~7초 소요
-#     starting_time = time.time()
-#     pokemon_data = []
+async def dev2(request):     # 약 6~7초 소요
+    starting_time = time.time()
+    pokemon_data = []
     
-#     async with aiohttp.ClientSession() as session:
-#         for num in range(1, 101):
-#             url = f"https://pokeapi.co/api/v2/pokemon/{num}"
-#             async with session.get(url) as res:
-#                 pokemon = await res.json()
-#                 pokemon_data.append([pokemon['name'], pokemon['sprites']['other']['official-artwork']['front_default']])
-#     count = len(pokemon_data)
-#     total_time = time.time() - starting_time
-#     context = {'data': pokemon_data, 'count': count, 'time': total_time}
-#     return render(request, 'index.html', context)
+    async with aiohttp.ClientSession() as session:
+        for num in range(1, 101):
+            url = f"https://pokeapi.co/api/v2/pokemon/{num}"
+            async with session.get(url) as res:
+                pokemon = await res.json()
+                pokemon_data.append([pokemon['name'], pokemon['sprites']['other']['official-artwork']['front_default']])
+    count = len(pokemon_data)
+    total_time = time.time() - starting_time
+    context = {'data': pokemon_data, 'count': count, 'time': total_time}
+    return render(request, 'index.html', context)
 
 
 # asyncio.gather() run coroutines concurrently
@@ -46,7 +53,7 @@ async def get_data(session, url):
         pokemon_data = await res.json()
         return pokemon_data
 
-async def dev1(request):    # 약 1~2초 소요
+async def dev3(request):    # 약 1~2초 소요
     starting_time = time.time()
     
     actions =[]
